4eb4ebdd2ed49c4b4fea8282099e95737e480d9c
Adeyeye Niyi Festus <adeyeyeniyi2@gmail.com>
2025-01-09 13:29:57 +0100

    "about to run the hamberger animation for varing versions"

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
           raw: "(min-aspect-ratio: 3/2)",
         }, // target wide screens (e.g., 16:9 or wider)
         tallscreen: {
-          raw: "(min-aspect-ratio: 1/2)",
+          raw: "(min-aspect-ratio: 13/20)",
         }, // tall screens (e.g., 9:16 or taller)
       },
     },
